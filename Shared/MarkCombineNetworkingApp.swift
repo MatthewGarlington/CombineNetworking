484bd3ccbf4386bae941168a176dbf6d861cce0c
Matthew Garlington <sbmatthewG@gmail.com>
2022-02-21 17:07:58 -0500

    Initial Commit

---
+++ Shared/MarkCombineNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkCombineNetworkingApp.swift
+//  Shared
+//
+//  Created by Matthew Garlington on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MarkCombineNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
